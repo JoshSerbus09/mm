b2aa29899b4915c3337a66f6384c05b5ae80ce04
Josh Serbus <JoshSerbus09@gmail.com>
2013-08-20 17:26:36 -0500

    I forgot to add files on last commit. Face applied to palm.

---
+++ mm/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mm::Application.config.secret_token = '1089ab906bda43a38a9e19c44d2f78aecd58a4e5564819f8f6312adef38de8b281260af13b7a14d3809dd9ffc8cdead12456fdb2d574ee5c3d37ac6c50414603'
